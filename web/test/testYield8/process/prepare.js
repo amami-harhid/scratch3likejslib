afd1939d74fe2f66d9e6ef3ede67a616ab8be2f3
amami-harhid <amami-thida100@outlook.jp>
2025-02-10 17:02:02 +0900

    update

--- web/test/testYield8/process/prepare.js
+++ web/test/testYield8/process/prepare.js
@@ -2,5 +2,5 @@ export async function prepare() {
     P.stage = new P.Stage();
     P.stage.addImage( P.images.Jurassic );
     P.cat = new P.Sprite("Cat");
-//    P.cat.addImage( P.images.Cat );
+    P.cat.addImage( P.images.Cat );
 }
--- web/test/testYield8/process/setting.js
+++ web/test/testYield8/process/setting.js
@@ -1,9 +1,20 @@
 export async function setting() {
-
+    // JSのスコープ規則どおり
+    // setting()関数内のローカル変数を
+    // whenFlag()関数内で利用できる。
+    let x = 3;
     const y="xxxx"
 
     P.stage.whenFlag(async function(){
         console.log(y)
-        P.cat.addImage( P.images.Cat );
     });
+
+    P.cat.whenFlag(async function(){
+        await this.while(_=>x<1000, _=>{
+            console.log("-----"+x);
+            x+=1;
+            this.moveSteps(10);
+            this.ifOnEdgeBounds();
+        });
+    })
 }
