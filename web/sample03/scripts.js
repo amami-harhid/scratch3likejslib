7e5bc608ede9b0375ee6b7d0865b6dee379e74b1
amami-harhid <amami-thida100@outlook.jp>
2025-02-23 15:03:52 +0900

    update

--- web/sample03/scripts.js
+++ web/sample03/scripts.js
@@ -10,10 +10,17 @@ Pg.title = "【Sample03】旗クリックでずっと『終わるまで音を鳴
 Pg.preload = function() {
     this.Image.load('../assets/Jurassic.svg','Jurassic');
     this.Sound.load('../assets/Chill.wav','Chill');
+    /**
+     * Sound ロードで、サウンドでないとき エラーにしたいが？？
+     */
 }
 Pg.prepare = function() {
     St.stage = new Lib.Stage();
     St.stage.Image.add( Images.Jurassic );
+    /**
+     * St.stage.Image.add("Jurassic")の形が望ましい。
+     * Image/Images と2回書く意味が薄い。
+     */
 }
 Pg.setting = function() {
     // すぐに実行する。
