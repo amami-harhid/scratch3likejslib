07999d8cbd3937db7fe842733b28ee132c5a8cbe
amami-harhid <amami-thida100@outlook.jp>
2025-02-27 18:04:26 +0900

    update

--- web/sample22/scripts.js
+++ web/sample22/scripts.js
@@ -37,59 +37,57 @@ Pg.prepare = async function prepare() {
 
 Pg.setting = async function setting() {
 
-    stage.Event.whenFlag(async function( $this ){
-        await $this.Sound.add( Chill );
-        $this.Sound.setOption( Lib.SoundOption.VOLUME, 20 )
-        await $this.Control.forever( async _=>{
-            await $this.Sound.playUntilDone();
+    stage.Event.whenFlag(async function(){
+        await this.Sound.add( Chill+2 );
+        this.Sound.setOption( Lib.SoundOption.VOLUME, 20 )
+        await this.Control.forever( async _=>{
+            await this.Sound.playUntilDone();
         });
     })
 
-    cat.Event.whenFlag( async function( $this ){
-        $this.Control.forever( async _=>{
-            await $this.Sound.playUntilDone();
+    cat.Event.whenFlag( async function(){
+        this.Control.forever( async _=>{
+            await this.Sound.playUntilDone();
         });
     });
     
     // ネコにさわったらお話する
-    cat.Event.whenFlag( async function( $this ){
+    cat.Event.whenFlag( async function( ){
         const words = `なになに？`;
         const properties = {'pitch': 2, 'volume': 100}
-        $this.C.forever( async _=>{
-            if( $this.Sensing.isMouseTouching() ) {
-                $this.Looks.say(words);// フキダシを出す
-                await $this.Event.broadcastAndWait('SPEECH', words, properties, 'male');
-                $this.Looks.say(""); // フキダシを消す
+        this.C.forever( async _=>{
+            if( this.Sensing.isMouseTouching() ) {
+                this.Looks.say(words);// フキダシを出す
+                await this.Event.broadcastAndWait('SPEECH', words, properties, 'male');
+                this.Looks.say(""); // フキダシを消す
                 // 「送って待つ」を使うことで スピーチが終わるまで次のコードに進まない。
                 // スピーチは２つ同時にできないので、スプライトクリックのイベントと重なってしまう。
                 // 以下の「マウスタッチしている間、待つ」をして 「なになに？」のスピーチ開始を一旦とめる。
-                await Lib.waitWhile( ()=>$this.Sensing.isMouseTouching()); 
+                await Lib.waitWhile( ()=>this.Sensing.isMouseTouching()); 
             }else{
-                await $this.Event.broadcastAndWait('SPEECH_STOP');
+                await this.Event.broadcastAndWait('SPEECH_STOP');
             }
         });
     });
     // ネコをクリックしたらお話する
-    cat.Event.whenClicked(async function( $this ){
+    cat.Event.whenClicked(async function( ){
         const words = `そこそこ。そこがかゆいの。`;
         const properties = {'pitch': 1.7, 'volume': 500}
         // スピーチを止めるためのメッセージを送る
-        await $this.Event.broadcastAndWait('SPEECH_STOP');
+        await this.Event.broadcastAndWait('SPEECH_STOP');
         // スピーチさせるメッセージを送る
-        await $this.Event.broadcastAndWait('SPEECH', words, properties, 'female');
+        await this.Event.broadcastAndWait('SPEECH', words, properties, 'female');
     });
     
     /** スピーチのメッセージを受信したとき */
     cat.Event.whenBroadcastReceived('SPEECH', async function(words, properties, gender='male', locale='ja-JP') {
-        const $this = this;
         // speechAndWait に await をつけて、音声スピーチが終わるまで待つ。
-        await $this.Extensions.speechAndWait(words, properties, gender, locale);
+        await this.Extensions.speechAndWait(words, properties, gender, locale);
     });
     /** スピーチ停止のメッセージを受信したとき */
     cat.Event.whenBroadcastReceived('SPEECH_STOP', async function() {
-        const $this = this;
         // スピーチを全て停止する
-        $this.Extensions.speechStopAll();
+        this.Extensions.speechStopAll();
     });
 
 }
---
+++ web/test/testArrowFunction/test.js
@@ -0,0 +1,57 @@
+/*
+  アロー関数の判定
+*/
+
+class FunctionChecker {
+    /**
+     * アロー関数は prototypeを持たないことを利用して
+     * アロー関数であるか否かを判定する。
+     * @param {*} f 
+     * @returns True ( アロー関数 ), False ( 以外 )
+     */
+    static isArrowFunction( f ) {
+        if( f ) {
+            if(f.prototype == undefined) {
+                // アロー関数または async function
+                const str = f.toString();
+                console.log(str)
+                const isAsyncFunction = FunctionChecker.regexArrowFunction.test(str);
+                if(isAsyncFunction) return false;
+                return true;
+            }else{
+                // async でない function
+                return false;
+            }        
+        }
+        return false;
+    }
+    static get regexArrowFunction() {
+        return /^async\s+function\s*\([^\)]*\)\s*{/
+    }
+}
+
+const f01 = async function() {
+    
+    return true;
+}
+
+const f02 = function() {
+    
+    return true;
+}
+
+const f03 = async () => {
+    return true;
+}
+
+const f04 = async   () => {
+    return true;
+}
+
+console.log( "f01 is arrow ? = " + FunctionChecker.isArrowFunction( f01 ));
+   
+console.log( "f02 is arrow ? =" + FunctionChecker.isArrowFunction( f02 ));
+
+console.log( "f03 is arrow ? =" + FunctionChecker.isArrowFunction( f03 ));
+
+console.log( "f04 is arrow ? =" + FunctionChecker.isArrowFunction( f04 ));
