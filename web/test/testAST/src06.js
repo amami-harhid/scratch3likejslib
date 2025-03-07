26e7104c854e730be7dcc16997168a242ce5de2a
amami-harhid <amami-thida100@outlook.jp>
2025-03-07 18:14:39 +0900

    update

--- web/test/testAST/src06.js
+++ web/test/testAST/src06.js
@@ -29,4 +29,11 @@ const test07 =  async ()=>{
         yield;
     } while(true);
 }
-module.exports  = {test01,test02,test03,test04,test05,test06, test07};
+const test08 =  function(){
+    return __asyncGenerator(this, arguments, function* () {
+		let x = 0;
+	})
+}
+
+
+module.exports  = {test01,test02,test03,test04,test05,test06, test07,test08};
--- web/test/testAST/test4.js
+++ web/test/testAST/test4.js
@@ -4,11 +4,11 @@
  * ここで必要なライブラリ
  * npm install --save @babel/parser
  */
-const {test01,test02,test03,test04,test05,test06,test07} = require('./src06');
+const {test01,test02,test03,test04,test05,test06,test07,test08} = require('./src06');
 
 const { parse } = require("@babel/parser");
 
-const arr = [test01,test02,test03,test04,test05,test06,test07]
+const arr = [test01,test02,test03,test04,test05,test06,test07,test08]
 
 let testNo = 0;
 for(const test of arr){
@@ -17,14 +17,13 @@ for(const test of arr){
     const ast = parse(`const x = ${test.toString()}`);
     const functionInit = ast.program.body[0].declarations[0].init;
     console.log("------------")
-    if(functionInit.body.body.length>6){
-        console.log(functionInit.body.body[6])
-        console.log("------------")
-        console.log(functionInit.body.body[6].body)
-
-    }
+    console.log(ast.program);
     const arrowFunction = functionInit.type == "ArrowFunctionExpression";
     const generatorFunction = functionInit.generator;
     const asyncFunction = functionInit.async;
     console.log(`arrowFunction=${arrowFunction}, asyncFunction=${asyncFunction},generatorFunction=${generatorFunction}`);
 }
+
+const ast = parse(`const x = ${test08.toString()}`);
+console.log("------------")
+console.log(ast.program.body[0].declarations[0].init.body.body[0].argument.callee.name);
